e690898f886b464599294f51d3dc735e7391d9a7
Yakimax <sv1099912@gmail.com>
2023-10-02 15:38:58 +0530

    add

--- Day6/Challenge2/Main.java
+++ Day6/Challenge2/Main.java
@@ -1,6 +1,11 @@
 package Day6.Challenge2;
 
 public class Main {
+    public static void swap(int arr[],int i,int j){
+        int temp = arr[j] ;
+        arr[j] = arr[i] ;
+        arr[j] = temp ;
+    }
     public static void main(String[] args) {
         int arr[] = { -5, -2, 5, 2, 4, 7, 1, 8, 0, -8 };
         int mainPtr = 0 ;
@@ -9,11 +14,22 @@ public class Main {
             if(mainPtr%2 == 0 && arr[mainPtr] < 0 || mainPtr%2 != 0 && arr[mainPtr] > 0){
                 mainPtr++ ;
                 j = mainPtr ;
-            }else if (mainPtr%2 == 0 && arr[mainPtr] < 0 ){
-                mainPtr++ ;
-            }else if(){
-                
+            }else if (mainPtr%2 == 0 && arr[mainPtr] > 0 ){
+                if( arr[j] < 0 ){
+                    swap(arr,mainPtr,j) ;
+                }else{
+                    j++ ;
+                }
+            }else if(mainPtr%2 != 0 && arr[mainPtr] < 0){
+                if(arr[j] > 0){
+                    swap(arr,mainPtr,j) ;
+                }else{
+                    j++ ;
+                }
             }
+        }
+        for( int k = 0 ; k < arr.length ; k++ ){
+            System.out.println(arr[k]) ;
         }
     }
 }
