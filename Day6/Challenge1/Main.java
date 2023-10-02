e7287f7c6194480c73b0ac61edf13c5380adf4c9
Yakimax <sv1099912@gmail.com>
2023-10-02 09:12:07 +0530

    add

--- Day6/Challenge1/Main.java
+++ Day6/Challenge1/Main.java
@@ -1,7 +1,24 @@
 package Day6.Challenge1;
-
+import java.util.* ;
 public class Main {
     public static void main(String[] args) {
         System.out.println() ;
+        Scanner scn = new Scanner(System.in) ;
+        int n = scn.nextInt() ;
+        int []arr = new int[n]  ;
+        for(int i = 0 ; i  < n ; i++){
+            arr[i] = scn.nextInt() ;
+        }
+        scn.close();
+        int i = 0 ;
+        int j = arr.length-1 ;
+        while(i<j){
+            int temp = arr[i] ;
+            arr[i++] = arr[j] ;
+            arr[j--] = temp ;
+        }
+        for( i = 0 ; i < n ; i++){
+            System.out.print(arr[i] + "  ," ) ;
+        }
     }
 }
