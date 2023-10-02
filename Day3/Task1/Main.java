dc0fb2615373845089a7fdb3f4d09c447d9799c5
Yakimax <sv1099912@gmail.com>
2023-10-02 08:11:51 +0530

    add

--- Day3/Task1/Main.java
+++ Day3/Task1/Main.java
@@ -2,7 +2,7 @@ package Day3.Task1 ;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 130 ;
+        // int a = 130 ;
         // byte b =  (byte)a ;
         long l = 2147483670l ;
         int n = (int)l ;
