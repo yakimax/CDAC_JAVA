dac6b519c5c6118ebea082522f05acbdb84a9c96
Yakimax <sv1099912@gmail.com>
2023-10-02 11:16:19 +0530

    add

--- Day6/Challenge1/Main.java
+++ Day6/Challenge1/Main.java
@@ -1,6 +1,10 @@
 package Day6.Challenge1;
 import java.util.* ;
 public class Main {
+
+    public void reverseArr(int arr[]){
+
+    }
     public static void main(String[] args) {
         System.out.println() ;
         Scanner scn = new Scanner(System.in) ;
@@ -9,16 +13,17 @@ public class Main {
         for(int i = 0 ; i  < n ; i++){
             arr[i] = scn.nextInt() ;
         }
-        scn.close();
-        int i = 0 ;
-        int j = arr.length-1 ;
-        while(i<j){
-            int temp = arr[i] ;
-            arr[i++] = arr[j] ;
-            arr[j--] = temp ;
+        scn.close() ;
+        for(int i = 0 ; i  < n ; i++){
+            for(int j = 0 ; j < n-i ; j++){
+                int temp = arr[i] ;
+                arr[i] = arr[i+1] ;
+                arr[i+1] = temp ;
+                i++ ;
+            }
         }
-        for( i = 0 ; i < n ; i++){
-            System.out.print(arr[i] + "  ," ) ;
+        for(int i = 0 ; i < n ; i++){
+            System.out.print(arr[i] + "  " ) ;
         }
     }
 }
---
+++ Day6/Challenge2/Main.java
@@ -0,0 +1,19 @@
+package Day6.Challenge2;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = { -5, -2, 5, 2, 4, 7, 1, 8, 0, -8 };
+        int mainPtr = 0 ;
+        int j = 0 ;
+        while( mainPtr < arr.length ){
+            if(mainPtr%2 == 0 && arr[mainPtr] < 0 || mainPtr%2 != 0 && arr[mainPtr] > 0){
+                mainPtr++ ;
+                j = mainPtr ;
+            }else if (mainPtr%2 == 0 && arr[mainPtr] < 0 ){
+                mainPtr++ ;
+            }else if(){
+                
+            }
+        }
+    }
+}
