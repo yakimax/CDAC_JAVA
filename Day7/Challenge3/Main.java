87898011f5fe9c5d2a163640e3872820a2d1bf52
Yakimax <sv1099912@gmail.com>
2023-10-03 22:37:43 +0530

    add

--- Day7/Challenge3/Main.java
+++ Day7/Challenge3/Main.java
@@ -2,16 +2,16 @@ package Day7.Challenge3;
 
 public class Main {
     public static void main(String args[]){
-        int arr[] = { 1,2,4,5,6,7} ;
-        // for(int i = 0 ; i < 6 ; i++){
-        //     arr[i]-= i+1 ;
-        // }
-        // for(int i = 0 ; i < 6 ; i++){
-        //     if(arr[i]>0){
-        //         System.out.println(i+1) ;
-        //         break ;
-        //     }
-        // }
+        int arr[] = { 1,2,4,5,6,7 } ;
+        /*for(int i = 0 ; i < 6 ; i++){
+            arr[i]-= i+1 ;
+        }
+        for(int i = 0 ; i < 6 ; i++){
+            if(arr[i]>0){
+                System.out.println(i+1) ;
+                break ;
+            }
+        }*/
         for( int i = 0 ; i < 6 ; i++ ){
             
         }
