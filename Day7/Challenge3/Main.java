6832566f4bff3fba202d2519947b63529c9b325c
Yakimax <sv1099912@gmail.com>
2023-10-03 23:21:34 +0530

    add

--- Day7/Challenge3/Main.java
+++ Day7/Challenge3/Main.java
@@ -1,8 +1,9 @@
-package Day7.Challenge3;
-
+package Day7.Challenge3 ;
+import java.util.* ;
 public class Main {
     public static void main(String args[]){
-        int arr[] = { 1,2,4,5,6,7 } ;
+        // int arr[] = { 1,2,4,5,6,7 } ;
+        int arr[] = {-8,-1,1,2,3,4,-7,-5,-6,-2,-3,0} ;
         /*for(int i = 0 ; i < 6 ; i++){
             arr[i]-= i+1 ;
         }
@@ -12,8 +13,18 @@ public class Main {
                 break ;
             }
         }*/
+        // Using dataStructure HashSet
+        HashSet<Integer>hs = new HashSet<>() ;
+        int min = Integer.MAX_VALUE ;
         for( int i = 0 ; i < 6 ; i++ ){
-            
+            if(arr[i] < min){
+                min = arr[i] ;
+            }
+            hs.add(arr[i]) ;
         }
+        while(hs.contains(min+1)){
+            min++ ;
+        }
+        System.out.println(min+1) ;
     }
 }
