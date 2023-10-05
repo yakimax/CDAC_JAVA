7d4dfb980763020f4e0e939bf7874a0e57682e41
Yakimax <sv1099912@gmail.com>
2023-10-05 21:07:05 +0530

    addd

--- Day9/AssignmentPrev/BookLibrary/Library.java
+++ Day9/AssignmentPrev/BookLibrary/Library.java
@@ -4,9 +4,26 @@ public class Library {
     String title ;
     String author ;
     int price ;
-    Library(String title,String author ,int price){
+    Library(){
+        this.title = "" ;
+        this.author = "" ;
+        this.price = 0 ;
+    }
+    Library(String title,String author){
+        this() ;
         this.title = title ;
         this.author = author ;
+    }
+    Library(String title,String author ,int price){
+        this(title,author) ;
         this.price = price ; 
+    }
+    void displayBookDetails(){        
+        System.out.println(this.title) ;
+        System.out.println(this.author) ;
+        System.out.println(this.price) ;
+    }
+    void changePrice(int changedPrice){
+        this.price = changedPrice ;
     }
 }
