f81f1280fd2a17212e1e9683a10687f473d48db5
Yakimax <sv1099912@gmail.com>
2023-10-07 10:34:22 +0530

    add

---
+++ Day11/Stringargs/Main.java
@@ -0,0 +1,11 @@
+package Day11.Stringargs;
+
+public class Main {
+    public static void main() {
+        //compiler can compile class without main method
+        //compiler can compile class with static void main(String args[]){}
+        //compiler can compile class with public void main(String args[]){} 
+        //compiler can compile class with static void main(){}         ----> considered as default specifier
+        //compiler can compile class with static void main(String args[]){}
+    }
+}
