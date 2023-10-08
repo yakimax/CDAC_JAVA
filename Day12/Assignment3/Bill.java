fba61022ab63c1c0d59d7bf3155e84cd5a9fcaf2
Yakimax <sv1099912@gmail.com>
2023-10-08 22:28:31 +0530

    add

---
+++ Day12/Assignment3/Bill.java
@@ -0,0 +1,18 @@
+package Day12.Assignment3;
+
+public class Bill {
+    int billId ;
+    double amount ;
+    static final double TAX_RATE = 5 ;
+    static final double DISCOUNT_RATE = 10 ;
+    static final double SPECIAL_OFFER_DISCOUNT = 25 ;
+    void calculateTotalBill(){
+
+    }
+    void applyDiscount(){
+
+    }
+    void displayBill(){
+        
+    }
+}
---
+++ Day12/Assignment3/OrderManagementSystem.java
@@ -0,0 +1,18 @@
+package Day12.Assignment3;
+
+public class OrderManagementSystem {
+    MenuItem[] menuItems ;
+    MenuItem[] customerOrder ;
+    void viewMenu(){
+
+    }
+    void addToOrder(){
+
+    }
+    void displayOrderDetails(){
+
+    }
+    void displaySpecialOffers(){
+        
+    }
+}
