eea9bf495f98f6758572b37cd711aefe40fe68de
Yakimax <sv1099912@gmail.com>
2023-10-10 00:35:30 +0530

    add

--- Day13/Misc/Main.java
+++ Day13/Misc/Main.java
@@ -1,5 +1,7 @@
 package Day13.Misc ;
 
+import java.lang.management.OperatingSystemMXBean;
+
 class OperatingSystem{
     void designer(){
         System.out.println("Hey i was designed by general  motors") ;
@@ -24,11 +26,15 @@ class Windows extends OperatingSystem{
 
 public class Main {
 
+    public static void getInfo(OperatingSystem obj){
+        obj.designer() ;
+    }
 
     public static void main(String[] args) {
-        OperatingSystem os = new Windows() ;
-        os.getInfo(os) ;
-        os = new MacOs() ;
-        os.getInfo(os) ;
+        Windows os1 = new Windows() ;
+        getInfo(os1) ;
+        MacOs os2 = new MacOs() ;
+        getInfo(os2) ;
+        // MacOs m = new OperatingSystem() ;        //error 
     }
 }
