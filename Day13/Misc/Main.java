94f8e6ae35421581ebf7189ee0ad392a3f4a0726
Yakimax <sv1099912@gmail.com>
2023-10-09 20:47:15 +0530

    add

--- Day13/Misc/Main.java
+++ Day13/Misc/Main.java
@@ -1,22 +1,34 @@
-package Day13.Misc;
+package Day13.Misc ;
 
 class OperatingSystem{
     void designer(){
-        System.out.println("Hey i was ") ;
+        System.out.println("Hey i was designed by general  motors") ;
+    }
+    void getInfo(OperatingSystem obj){
+        obj.designer() ;
     }
 }
 
-class MacOs{
-
+class MacOs extends OperatingSystem{
+    void designer(){
+        System.out.println("Hey i was  designed by Apple") ;
+    }
 }
 
-class Windows{
-
+class Windows extends OperatingSystem{
+    void designer(){
+        System.out.println("Hey i was  designed by Microsoft") ;
+    }
 }
 
 
 public class Main {
+
+
     public static void main(String[] args) {
-        
+        OperatingSystem os = new Windows() ;
+        os.getInfo(os) ;
+        os = new MacOs() ;
+        os.getInfo(os) ;
     }
 }
