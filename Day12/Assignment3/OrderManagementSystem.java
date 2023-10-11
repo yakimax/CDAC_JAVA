79011bf3f143ff11d63259477ad612320fe5fb2c
Yakimax <sv1099912@gmail.com>
2023-10-11 11:00:39 +0530

    add

--- Day12/Assignment3/OrderManagementSystem.java
+++ Day12/Assignment3/OrderManagementSystem.java
@@ -2,20 +2,23 @@ package Day12.Assignment3 ;
 import Day12.Assignment3.* ;
 
 
-
 public class OrderManagementSystem {
     MenuItem[] menuItems ;
     MenuItem[] customerOrder ;
+    static int index = 0 ;
     void viewMenu(){
         for(int i = 0 ; i < menuItems.length ; i++){
-            System.out.println(i + " : " + menuItems[i]) ;
+            System.out.println(i + " . " + menuItems[i]) ;
         }
     }
-    void addToOrder(){
-        
+    void addToOrder(int i){
+        customerOrder[index] = customerOrder[i] ;
+        index++ ;
     }
     void displayOrderDetails(){
-         
+        for( int i = 0 ; i < customerOrder.length ; i++ ){
+            System.out.println(i + ". " + customerOrder[i]) ;
+        }
     }
     void displaySpecialOffers(){
         System.out.println("Todays Special discount is  : " Bill.SPECIAL_OFFER_DISCOUNT + "%  On selct Items") ;
