1f57ebfb17ff8db09193b70be80b41beca9c82c2
Yakimax <sv1099912@gmail.com>
2023-10-12 09:49:57 +0530

    add

---
+++ Day16/ExceptionHandling/ThrowsKeyword.java
@@ -0,0 +1,19 @@
+package Day16.ExceptionHandling ;
+
+import java.io.IOException;
+
+public class ThrowsKeyword {
+
+    int myFun() throws ArithmeticException {
+        int a = 0 ;
+        int b = 10 ;
+        return b/a ;
+    }
+    public static void main(String[] args) {
+        try{
+            
+        }catch(ArithmeticException ae){
+
+        }
+    }
+}
