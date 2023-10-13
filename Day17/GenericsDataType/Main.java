80e55a77953a37b068d71cbd4966b704cc3b4393
Yakimax <sv1099912@gmail.com>
2023-10-13 21:18:42 +0530

    add

---
+++ Day17/GenericsDataType/Main.java
@@ -0,0 +1,19 @@
+package Day17.GenericsDataType ;
+
+class GenerericDemo <T> {
+    T a ;
+    T b ;
+    GenerericDemo(){}
+    GenerericDemo(T a , T b ){
+        this.a = a ;
+        this.b = b ;
+    }
+    
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
