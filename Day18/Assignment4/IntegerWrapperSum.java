f1f3a79ef57b2ce8d936bf9f27a435750d0965fd
Yakimax <sv1099912@gmail.com>
2023-10-14 11:20:47 +0530

    add

---
+++ Day18/Assignment4/IntegerWrapperSum.java
@@ -0,0 +1,18 @@
+package Day18.Assignment4 ;
+
+
+public class IntegerWrapperSum {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+class IntegerWrapper<T> {
+    T a ;
+    T b ;
+    IntegerWrapper(T a,T b){
+        this.a = a ;
+        this.b = b ;
+    }
+}
---
+++ Day18/wrapperPractice/Main.java
@@ -0,0 +1,21 @@
+package Day18.wrapperPractice ;
+
+
+class IntegerWrapper<T>{
+    T a ;
+    T b ;
+    IntegerWrapper(T a, T b){
+        this.a = a ;
+        this.b = b ;
+    }
+
+}
+
+
+public class Main {
+    Integer val = new Integer(100) ;
+    Integer val2 = 100 ;
+    
+    System.out.println(val1) ;
+    System.out.println(val2) ;
+}
