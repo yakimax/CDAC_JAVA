d40ccb75268b412f3cc2de6240fceafafae33dbe
Yakimax <sv1099912@gmail.com>
2023-10-17 12:14:24 +0530

    add

--- Day21/ExceptionHandling/TryWithResources/Main.java
+++ Day21/ExceptionHandling/TryWithResources/Main.java
@@ -1,7 +1,20 @@
-package Day21.ExceptionHandling.TryWithResources ; 
+package Day21.ExceptionHandling.TryWithResources ;
+
+import java.io.* ;
+
+class Excp {
+    
+}
 
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) { 
+        int a = 1/0 ;
+        try{
+        // try( FileReader f1 = new FileReader("File.txt") ; PrintWriter p1 = new PrintWriter("File.txt") ) {
+        //     System.out.println("Hello") ;
+        // }
+        }catch(Exception e){
+            System.out.println(e.getMessage()) ;
+        }
     }
 }
